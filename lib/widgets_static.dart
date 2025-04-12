3f968f4bab9634c08664accaf9cd62a2ff6672f3
Barrie Hadfield <barrie+github@tyger.com>
2025-04-12 07:04:47 +0100

    working example in static passing attributes in view

--- lib/widgets_static.dart
+++ lib/widgets_static.dart
@@ -2,8 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:js_interop';
+
+import 'dart:ui';
+import 'dart:ui_web' as ui_web;
+
 import 'package:flutter/widgets.dart';
+
 import 'src/widgets.dart';
+
+@JS()
+@staticInterop
+class InitialData {
+  // Leave empty or include static interop properties
+}
+
+// Define an extension to add getters
+extension InitialDataExtension on InitialData {
+  external String get greeting; // Replace 'greeting' with the actual property
+}
 
 void main() {
   runWidget(MultiViewApp(
@@ -18,13 +35,42 @@ class HelloWorld extends StatelessWidget {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.ltr,
-      child: Center(
-        child: Text(
-          'Hello World from View#${View.of(context).viewId}\n'
-          'Logical ${MediaQuery.sizeOf(context)}\n'
-          'DPR: ${MediaQuery.devicePixelRatioOf(context)}'
-        ),
-      ),
+      child: _viewSelector(context),
     );
+  }
+
+  Widget _viewSelector(BuildContext context) {
+    final FlutterView view = View.of(context);
+
+    final InitialData initialData =
+        ui_web.views.getInitialData(view.viewId)! as InitialData;
+
+    return switch (View.of(context).viewId) {
+      1 => _viewOne(context, initialData),
+      2 => _viewTwo(context, initialData),
+      _ => const Center(
+          child: Text('Unknown view'),
+        ),
+    };
+  }
+
+  Center _viewOne(BuildContext context, InitialData initialData) {
+    return Center(
+        child: Text(
+      'VIEW 1 from View#${View.of(context).viewId}\n'
+      'Logical ${MediaQuery.sizeOf(context)}\n'
+      'Greeting: ${initialData.greeting}\n'
+      'Host Element: ${ui_web.views.getHostElement(View.of(context).viewId)}\n',
+    ));
+  }
+
+  Center _viewTwo(BuildContext context, InitialData initialData) {
+    return Center(
+        child: Text(
+      'VIEW 2 from View#${View.of(context).viewId}\n'
+      'Logical ${MediaQuery.sizeOf(context)}\n'
+      'Greeting: ${initialData.greeting}\n'
+      'Host Element: ${ui_web.views.getHostElement(View.of(context).viewId)}\n',
+    ));
   }
 }
